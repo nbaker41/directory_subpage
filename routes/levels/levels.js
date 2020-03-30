a1f7c705dc1aba3d4c925838a1032859cee6f5dd
nbaker41 <58822482+nbaker41@users.noreply.github.com>
2020-03-30 08:24:14 -0400

    Update levels.js

--- routes/levels/levels.js
+++ routes/levels/levels.js
@@ -30,78 +30,78 @@
 
 
 
-			$(function() 
-            {
+			// $(function() 
+            // {
 
-                //overwrite jquery to match any case (by making uppercase)
-                jQuery.expr[':'].contains = function(a, i, m) {
-                return jQuery(a).text().toUpperCase()
-                .indexOf(m[3].toUpperCase()) >= 0;
-                };
-
-
-
-            var deactivateAll=function()
-                    {
-                    $('.roomHighlight').removeClass('active');
-                    $('.directory a').removeClass('active');
-                    $('g').removeClass('hasActiveCircle');
-                    }    
-
-            var hideShowHeadings=function()
-                    {
-                    if($('.rooms .show').length >0)
-                        {$('#roomsHeading').addClass('show');}
-                    else{$('#roomsHeading').removeClass('show');}
-                    if($('.people .show').length >0)
-                        {$('#peopleHeading').addClass('show');}
-                    else{$('#peopleHeading').removeClass('show');}
+            //     //overwrite jquery to match any case (by making uppercase)
+            //     jQuery.expr[':'].contains = function(a, i, m) {
+            //     return jQuery(a).text().toUpperCase()
+            //     .indexOf(m[3].toUpperCase()) >= 0;
+            //     };
 
 
-                    }    
 
-            $('.directory .rooms a').on( "click", function(e) 
-                    {
-                    e.preventDefault();
-                    var target = $(this).attr('href');
-                    deactivateAll();
+            // var deactivateAll=function()
+            //         {
+            //         $('.roomHighlight').removeClass('active');
+            //         $('.directory a').removeClass('active');
+            //         $('g').removeClass('hasActiveCircle');
+            //         }    
 
-                    $('#'+target).addClass('active')
-                    $(this).addClass('active')
-                    $('.map').animate({scrollTop: $("#"+target).offset().top});
-
-                    });
-
-             $('.directory .people a').on( "click", function(e) 
-                    {
-                    e.preventDefault();
-                    var target = $(this).attr('href');
-                    deactivateAll();
+            // var hideShowHeadings=function()
+            //         {
+            //         if($('.rooms .show').length >0)
+            //             {$('#roomsHeading').addClass('show');}
+            //         else{$('#roomsHeading').removeClass('show');}
+            //         if($('.people .show').length >0)
+            //             {$('#peopleHeading').addClass('show');}
+            //         else{$('#peopleHeading').removeClass('show');}
 
 
-                    $(this).addClass('active')
-                    $('.map').animate({scrollTop: $("#"+target).offset().top}, 200);
-                    $("#"+target).addClass('hasActiveCircle')
+            //         }    
 
-                    });
-            $('#searchBox').on( "input", function(e) 
-                    {
-                    var searchMatch = $(this).val();
-                    console.log( searchMatch);
+            // $('.directory .rooms a').on( "click", function(e) 
+            //         {
+            //         e.preventDefault();
+            //         var target = $(this).attr('href');
+            //         deactivateAll();
 
-                    if(searchMatch =="")
-                        {
-                        $("li").addClass("show");
-                        }
-                    else
-                        {
-                        $("li").removeClass("show");
-                        $("li:contains("+searchMatch+")").addClass("show");
-                        hideShowHeadings();
-                        }
+            //         $('#'+target).addClass('active')
+            //         $(this).addClass('active')
+            //         $('.map').animate({scrollTop: $("#"+target).offset().top});
+
+            //         });
+
+            //  $('.directory .people a').on( "click", function(e) 
+            //         {
+            //         e.preventDefault();
+            //         var target = $(this).attr('href');
+            //         deactivateAll();
+
+
+            //         $(this).addClass('active')
+            //         $('.map').animate({scrollTop: $("#"+target).offset().top}, 200);
+            //         $("#"+target).addClass('hasActiveCircle')
+
+            //         });
+            // $('#searchBox').on( "input", function(e) 
+            //         {
+            //         var searchMatch = $(this).val();
+            //         console.log( searchMatch);
+
+            //         if(searchMatch =="")
+            //             {
+            //             $("li").addClass("show");
+            //             }
+            //         else
+            //             {
+            //             $("li").removeClass("show");
+            //             $("li:contains("+searchMatch+")").addClass("show");
+            //             hideShowHeadings();
+            //             }
                     
-                    });
-			});
+            //         });
+			// });
 			
 
 			
